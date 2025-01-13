619b4fe94e8b1d73f2d9009c9aa8bc303a0eb1c7
Andres Gonzalez <andresfelipechiscagonzalez@gmail.com>
2025-01-13 11:27:16 -0600

    Enemy Movement
    Enemy Movement. Will work on combat system

--- assets/killzone.gd
+++ assets/killzone.gd
@@ -3,9 +3,11 @@ extends Area2D
 @onready var timer: Timer = $Timer
 
 # Signal when a body enters the Area2D
+
 func _on_body_entered(body: Node2D) -> void:
 	print("You died!")
 	timer.start()
+
 
 # Signal when the timer times out
 func _on_timer_timeout() -> void:
